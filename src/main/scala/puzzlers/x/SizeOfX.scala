2ce3ca84c0e4a7593c59aed8d2c2043cf69b35d1
Bill Carlson <bill@coacoas.net>
2013-10-08 00:48:14 -0400

    Tired now. Basic structure is there, but for a couple tests that are failing. Getting a stack overflow error on the board traversal, and it looks like my filtering isn't working quite right.

--- src/main/scala/puzzlers/x/SizeOfX.scala
+++ src/main/scala/puzzlers/x/SizeOfX.scala
@@ -1,6 +1,9 @@
 package puzzlers.x
 
-case class Position(val x: Int, val y: Int)
+case class Position(val x: Int, val y: Int) {
+  def +(other: Position) = Position(x + other.x, y + other.y)
+  def -(other: Position) = Position(x - other.x, y - other.y)
+}
 
 case class Board(val board: Vector[Vector[Int]], upperLeft: Position, width: Int, height: Int) {
   def bottomRight = Position(upperLeft.x + width - 1, upperLeft.y + height - 1)
@@ -9,16 +12,39 @@ case class Board(val board: Vector[Vector[Int]], upperLeft: Position, width: Int
 
   def corners = Vector(upperLeft, upperRight, bottomLeft, bottomRight).map(valueAt _)
 
-  val isValid = width >= 0 && height >= 0
+  val isEmpty = width <= 0 || height <= 0
   val isSquare = width == height
-  def isX: Boolean = isValid && isSquare &&
-    (width == 0 ||
-      (width == 1 && (valueAt(upperLeft) == 1)) ||
-      (corners.forall(_ == 1)) &&
-      subBoard(Position(upperLeft.x + 1, upperLeft.y + 1), width - 2, height - 2).isX)
+  def isX: Boolean =
+    isSquare &&
+      (!isEmpty) &&
+      ((width == 1 && (valueAt(upperLeft) == 1)) ||
+        (corners.forall(_ == 1)) && centerBoard.isX)
+
+  def centerBoard = subBoard(upperLeft + Position(1, 1), width - 2, height - 2)
 
   def valueAt(p: Position) = board(p.x)(p.y)
   def subBoard(newUpperLeft: Position, w: Int, h: Int) = new Board(board, newUpperLeft, w, h)
+
+  def immediateSubBoards: Stream[Board] = (subBoard(upperLeft, width - 1, height) #::
+    subBoard(upperLeft, width, height - 1) #::
+    subBoard(upperLeft + Position(1, 0), width - 1, height) #::
+    subBoard(upperLeft + Position(0, 1), width, height - 1) #:: Stream.empty[Board]).filter(b => !b.isEmpty)
+
+  def subBoardsSkipVisited(visited: Set[Board]): Stream[Board] = immediateSubBoards.filter(!visited.contains(_))
+
+  lazy val traverse: Stream[Board] = {
+    def from(initial: Stream[Board], explored: Set[Board]): Stream[Board] = {
+      val (nextInitial, nextExplored) = initial.foldLeft((Stream.empty[Board], explored)) {
+        case ((acc, exploredPlus), e) =>
+          println(exploredPlus)
+          println(e)
+          println("---------------------------")
+          (acc #::: e.subBoardsSkipVisited(exploredPlus), exploredPlus + e)
+      }
+      initial #::: from(nextInitial, nextExplored)
+    }
+    from(Stream(this), Set())
+  }
 }
 
 object Board {
@@ -31,8 +57,15 @@ object Board {
       width = width,
       height = height)
   }
-}
 
-class SizeOfX {
-
+  def traverse(board: Board): Stream[Board] = {
+    def from(initial: Stream[Board], explored: Set[Board]): Stream[Board] = {
+      val (nextInitial, nextExplored) = initial.foldLeft((Stream.empty[Board], explored)) {
+        case ((acc, exploredPlus), e) =>
+          (acc #::: e.subBoardsSkipVisited(exploredPlus), exploredPlus + e)
+      }
+      initial #::: from(nextInitial, nextExplored)
+    }
+    from(Stream(board), Set())
+  }
 }
--- src/test/scala/puzzlers/x/BoardTest.scala
+++ src/test/scala/puzzlers/x/BoardTest.scala
@@ -14,6 +14,9 @@ class BoardTest extends FunSuite with Matchers {
   val board1 = Vector(
       Vector(1))
   
+  val x2 = Vector(
+      Vector(1,1),
+      Vector(1,1))
   val x3 = Vector(
       Vector(1,0,1),
       Vector(0,1,0),
@@ -40,6 +43,22 @@ class BoardTest extends FunSuite with Matchers {
     Board(x3).isSquare should be (true)
   }
   
+  test("A two-by-two board is square") { 
+    Board(x2).isSquare should be (true)
+  }
+  
+  test("A two-by-two's center board is empty") { 
+    Board(x2).centerBoard.isEmpty should be (true)
+  }
+  
+  test("An empty board is not an X") { 
+    Board(emptyBoard).isX should be (false)
+  }
+  
+  ignore("A two-by-two board of ones is not an X") { 
+    Board(x2).isSquare should not be (true)
+  }
+  
   test("A three-by-two board is not square") { 
     Board(notSquare).isSquare should be (false)
   }
@@ -54,5 +73,45 @@ class BoardTest extends FunSuite with Matchers {
   
   test("A board with 1s in the corners only is not an X") { 
     Board(corners).isX should be (false)
+  }
+  
+  test("Empty boards should have no immediate children") { 
+    Board(emptyBoard).immediateSubBoards should equal (Stream.empty)
+  }
+  
+  test("One-element boards have no immeidate children") { 
+    Board(board0).immediateSubBoards.force.length should equal (0)
+  }
+  
+  test("1 x 2 board has two immediate children") { 
+    Board(Vector(Vector(1,1))).immediateSubBoards.force.length should equal (2)
+  }
+  
+  test("2 x 1 board has two immediate children") { 
+    Board(Vector(Vector(1), Vector(1))).immediateSubBoards.force.length should equal (2)
+  }
+
+  test("2 x 2 board has four immediate children") { 
+    Board(Vector(Vector(0,1), Vector(1,0))).immediateSubBoards.map(b => (b.width, b.height)) should 
+      equal (Stream((1,2),(2,1),(1,2),(2,1)))
+  }
+  
+  test("Filtering already visited children") {
+    val matrix = Vector(Vector(1,1))
+    val b = Board(matrix)
+    
+    b.subBoardsSkipVisited(Set(Board(matrix, Position(1,0), 1, 1))).force should 
+    equal (Stream(Board(matrix, Position(0, 0), 1, 1)))
+  }
+  
+//  test("A 2x2 board with all children has nine elements") { 
+//	val matrix = Vector(Vector(1,1),Vector(1,1))
+//	val b = Board(matrix)
+//    b.traverse.force.length should equal (9)
+//  }
+  
+  test("Immediate sub-boards returns the correct values") { 
+    val boards = Board(x3).immediateSubBoards
+    boards.length should equal (4)
   }
 }
