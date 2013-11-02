ba4e20e7974179d04e9fae85ba19785a3f452d87
Bill Carlson <bill@coacoas.net>
2013-11-02 16:34:19 -0400

    Just cleaning up a little

--- src/main/scala/puzzlers/coins/CoinChange.scala
+++ src/main/scala/puzzlers/coins/CoinChange.scala
@@ -7,14 +7,12 @@ object CoinChange {
     def update[K,V](m: Map[K,V], k: K)(f: V => V) = m.updated(k, f(m(k)))
     def inc(m: Map[Int,Int], k: Int) = update(m, k)(_ + 1)
     
-    def loop(acc: Map[Int, Int], remainingDenominations: Vector[Int], remainingAmount: Int): Vector[Map[Int,Int]] = { 
-      if (remainingAmount == 0) Vector(acc)
-      else if (remainingDenominations.isEmpty) Vector.empty
-      else if (remainingDenominations.head > remainingAmount) loop(acc, remainingDenominations.tail, remainingAmount)
-      else { 
-        val d = remainingDenominations.head
-        loop(inc(acc, d), remainingDenominations, remainingAmount - d) ++ loop(acc, remainingDenominations.tail, remainingAmount)
-      }
+    def loop(acc: Map[Int, Int], coins: Vector[Int], remaining: Int): Vector[Map[Int,Int]] = { 
+      if (remaining == 0) Vector(acc)
+      else if (coins.isEmpty) Vector.empty
+      else if (coins.head > remaining) loop(acc, coins.tail, remaining)
+      else loop(inc(acc, coins.head), coins, remaining - coins.head) ++ 
+           loop(acc, coins.tail, remaining)
     }
     loop(Map.empty.withDefaultValue(0), denominations, amount)
   }
