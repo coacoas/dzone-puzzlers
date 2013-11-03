87681f5e6eb9d2b7344b32c3cb11bff792c59b86
Bill Carlson <bill@coacoas.net>
2013-11-02 23:03:40 -0400

    Changed Vector to List for the change combinations puzzler.

--- src/main/scala/puzzlers/coins/CoinChange.scala
+++ src/main/scala/puzzlers/coins/CoinChange.scala
@@ -1,19 +1,16 @@
 package puzzlers.coins
 
 object CoinChange {
-  val denominations = Vector(25, 10, 5, 1)
-  
   def change(amount: Int) = { 
-    def update[K,V](m: Map[K,V], k: K)(f: V => V) = m.updated(k, f(m(k)))
-    def inc(m: Map[Int,Int], k: Int) = update(m, k)(_ + 1)
+    def inc(m: Map[Int,Int], k: Int) = m.updated(k, m(k) + 1)
     
-    def loop(acc: Map[Int, Int], coins: Vector[Int], remaining: Int): Vector[Map[Int,Int]] = { 
+    def loop(acc: Map[Int, Int], coins: List[Int], remaining: Int): Vector[Map[Int,Int]] = { 
       if (remaining == 0) Vector(acc)
       else if (coins.isEmpty) Vector.empty
       else if (coins.head > remaining) loop(acc, coins.tail, remaining)
       else loop(inc(acc, coins.head), coins, remaining - coins.head) ++ 
            loop(acc, coins.tail, remaining)
     }
-    loop(Map.empty.withDefaultValue(0), denominations, amount)
+    loop(Map.empty.withDefaultValue(0), List(25, 10, 5, 1), amount)
   }
 }
